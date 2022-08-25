d372be01968021d148e1146bcb1a254eb89ed537
Mr. HAPPY <94671185+Happy-76@users.noreply.github.com>
2022-08-25 20:59:42 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     int n,m,sum=0,add=0;     cin>>n>>m;     int a[n][m];     for(int i=0;i<n;i++){         for(int j=0;j<m;j++){             cin>>a[i][j];         }     }     for(int i=0;i<n;i++){         for(int j=0;j<m;j++)         sum+=a[i][j];     }     for(int i=1;i<n-1;i++){         for(int j=1;j<m-1;j++)         add+=a[i][j];     }
        cout<<sum-add<<endl;
        return 0;
    }

---
+++ Sum_of_all_boundary_values_in_a_matrix.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,m,sum=0,add=0;
+    cin>>n>>m;
+    int a[n][m];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>a[i][j];
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++)
+        sum+=a[i][j];
+    }
+    for(int i=1;i<n-1;i++){
+        for(int j=1;j<m-1;j++)
+        add+=a[i][j];
+    }
+    
+    cout<<sum-add<<endl;
+    return 0;
+}
