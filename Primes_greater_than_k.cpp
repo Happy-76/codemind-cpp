a25824431de0bf2815f3e9353c9157ef4e0ee552
Mr. HAPPY <94671185+Happy-76@users.noreply.github.com>
2022-09-23 01:14:37 +0530

    #include<bits/stdc++.h> using namespace std; bool prime(int n,int g){     int e =sqrt(n);     if(n==1 || n<g)return false;     for(int i=2;i<=e;i++){         if(n%i==0) return false;     }     return true; } int main(){     int n,c=0,o=0,g;     cin>>n;     int arr[n];     for(int i=0;i<n;i++){         cin>>arr[i];     }     cin>>g;     for(int j=0;j<n;j++){         if(prime(arr[j],g)){             c++;         }     }     cout<<c;     return 0; }

---
+++ Primes_greater_than_k.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool prime(int n,int g){
+    int e =sqrt(n);
+    if(n==1 || n<g)return false;
+    for(int i=2;i<=e;i++){
+        if(n%i==0) return false;
+    }
+    return true;
+}
+int main(){
+    int n,c=0,o=0,g;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cin>>g;
+    for(int j=0;j<n;j++){
+        if(prime(arr[j],g)){
+            c++;
+        }
+    }
+    cout<<c;
+    return 0;
+}
