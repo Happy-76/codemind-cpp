67958c9704c2c1c4e6ddffe9d776d08cc87be019
Mr. HAPPY <94671185+Happy-76@users.noreply.github.com>
2022-09-24 20:05:04 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     int n,c=0;     cin>>n;     int arr[n];     for(int i=0;i<n;i++){         cin>>arr[i];     }     for(int i=0;i<n;i++){         if(i!=n-1 && i!=0){             if(arr[i-1]%2==0 && arr[i+1]%2!=0)             c++;             else if(arr[i-1]%2!=0 && arr[i+1]%2==0)             c++;         }         else if(i==0){             continue;         }         else if(i==n-1){             continue;         }     }     cout<<c;     return 0; }

---
+++ count_numbers_between_even_and_odd_numbers_or_vice_versa.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,c=0;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        if(i!=n-1 && i!=0){
+            if(arr[i-1]%2==0 && arr[i+1]%2!=0)
+            c++;
+            else if(arr[i-1]%2!=0 && arr[i+1]%2==0)
+            c++;
+        }
+        else if(i==0){
+            continue;
+        }
+        else if(i==n-1){
+            continue;
+        }
+    }
+    cout<<c;
+    return 0;
+}
